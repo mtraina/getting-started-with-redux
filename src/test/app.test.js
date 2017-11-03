3555472a072f7f8a20afcdc40789fa833b02be2e
mtraina <matteo.traina.mail@gmail.com>
2017-11-03 17:44:45 +0100

    refactoring: if else to switch and default arg

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,13 +1,14 @@
 import expect from 'expect';
 
-function counter(state, action){
-    if(typeof state === 'undefined') return 0;
-
-    if(action.type === 'INCREMENT'){
-        return state + 1;
-    } else if(action.type === 'DECREMENT'){
-        return state  - 1
-    } else return state; 
+function counter(state = 0, action){
+    switch(action.type){
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
 }
 
 expect(
