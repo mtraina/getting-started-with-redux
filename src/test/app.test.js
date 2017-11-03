26e6fafbfbeb7b95a7a42bfd5628d5176fb371aa
mtraina <matteo.traina.mail@gmail.com>
2017-11-03 17:38:18 +0100

    assertions added

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -11,3 +11,15 @@ function counter(state, action){
 expect(
     counter(0, {type: 'INCREMENT'})
 ).toEqual(1);
+
+expect(
+    counter(1, {type: 'INCREMENT'})
+).toEqual(2);
+
+expect(
+    counter(1, {type: 'DECREMENT'})
+).toEqual(0);
+
+expect(
+    counter(2, {type: 'DECREMENT'})
+).toEqual(1);
